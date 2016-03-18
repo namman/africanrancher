8b5978bd94502384e4aab4fe45649bf34e6a3376
Nicholas Miller <nick@nickmiller.com.au>
2016-03-18 12:37:54 +1100

    bovines grid showing some data

--- src/africanrancher/wwwroot/js/angularModules/bovinesController.js
+++ src/africanrancher/wwwroot/js/angularModules/bovinesController.js
@@ -5,17 +5,63 @@
         .module('app')
         .controller('bovinesController', bovinesController);
 
-    bovinesController.$inject = ['$location','$scope']; 
+    bovinesController.$inject = ['$location','$scope','$http']; 
 
-    function bovinesController($location,$scope) {
+    function bovinesController($location,$scope,$http) {
         /* jshint validthis:true */
-        var vm = this;
-        vm.title = 'bovinesController';
 
-        $scope.title = "Test title";
+        var bovinesGridViewModel = function() {
+            var bovineKendoModel = kendo.data.Model.define({
+                fields: {
+                    'Id': {
+                        editable: false,
+                        type: 'number'
+                    },
+                    'EarTag': {
+                        editable: true,
+                        type: 'string'
+                    },
+                },
+                id: 'Id'
+            });
+
+            var bovinesDataSource = new kendo.data.DataSource( {
+                schema: {
+                    model: bovineKendoModel
+                },
+                transport: {
+                    read: {
+                        datatype: 'json',
+                        url: 'api/bovines'
+                    }
+                }
+            });
+
+            var options = {
+                columns: [
+                    {
+                        field: "Id",
+                        title: "Id"
+                    },
+                    {
+                        field: 'EarTag',
+                        title: 'Ear Tag'
+                    }
+                ],
+                dataSource: bovinesDataSource
+
+            }
+            
+            return {
+                options: options
+            }
+
+        }();
 
         activate();
 
-        function activate() { }
+        function activate() {
+            $scope.bovinesGridOptions = bovinesGridViewModel.options;
+        }
     }
 })();
