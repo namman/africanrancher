7286e0f524a7cb87c1e5498466b1c78891c91b38
Nicholas Miller <nick@nickmiller.com.au>
2016-03-18 11:18:24 +1100

    add kendo js and wire up angular1.5

--- src/africanrancher/wwwroot/_references.js
+++ src/africanrancher/wwwroot/_references.js
@@ -1,3 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/angularmodules/app.js" />
+/// <reference path="js/angularModules/bovinesController.js" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/angular/angular.js" />
---
+++ src/africanrancher/wwwroot/js/angularModules/bovinesController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('bovinesController', bovinesController);
+
+    bovinesController.$inject = ['$location','$scope']; 
+
+    function bovinesController($location,$scope) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'bovinesController';
+
+        $scope.title = "Test title";
+
+        activate();
+
+        function activate() { }
+    }
+})();
